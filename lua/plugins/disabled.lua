7faef859139ed42cecba2d45c46c2d905441a569
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2023-11-23 08:07:08 -0300

    [disable] catppuccine

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   -- disable context
   { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  -- disable catppuccin
+  { "catppuccin/nvim", name = "catppuccin", enabled = false },
 }
