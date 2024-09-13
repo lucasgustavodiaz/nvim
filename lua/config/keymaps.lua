75cb0b3ff95ecce963110904655731efc2e27090
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2024-09-13 11:53:20 -0300

    Update the Neovim setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,19 +5,11 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
--- Do things without affecting the registers
-keymap.set("n", "x", '"_x')
-keymap.set("n", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>P", '"0P')
-keymap.set("v", "<Leader>p", '"0p')
-keymap.set("n", "<Leader>c", '"_c')
-keymap.set("n", "<Leader>C", '"_C')
-keymap.set("v", "<Leader>c", '"_c')
-keymap.set("v", "<Leader>C", '"_C')
-keymap.set("n", "<Leader>d", '"_d')
-keymap.set("n", "<Leader>D", '"_D')
-keymap.set("v", "<Leader>d", '"_d')
-keymap.set("v", "<Leader>D", '"_D')
+-- Do not yank with x
+keymap.set('n', 'x', '"_x')
+
+-- Do not yank with d
+keymap.set('n', 'd', '"_d')
 
 -- Increment/decrement
 keymap.set("n", "+", "<C-a>")
