849877b2a3afe9d7ba778e8a4604865f1bb3c4d4
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2024-09-15 18:43:20 -0300

    add OS detection and config

--- init.lua
+++ init.lua
@@ -7,4 +7,16 @@ _G.dd = function(...)
 end
 vim.print = _G.dd
 
+local os = vim.loop.os_uname().sysname
+
+if os == "Darwin" then
+  require('macos')
+elseif os == "Linux" then
+  require('linux')
+elseif os == "Windows_NT" then
+  require('luks.windows')
+else
+  error("Unknown OS")
+end
+
 require("config.lazy")
