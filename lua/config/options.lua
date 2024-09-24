6f5fae1a659a45039e017a874d24727a61c149d9
Lucas Diaz <lucasgustavodiaz@hotmail.com>
2024-09-24 13:30:00 -0300

    fix: initial jump

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -54,9 +54,9 @@ vim.opt.formatoptions:append({ "r" })
 vim.cmd([[au BufNewFile,BufRead *.astro setf astro]])
 vim.cmd([[au BufNewFile,BufRead Podfile setf ruby]])
 
-if vim.fn.has("nvim-0.8") == 1 then
-	vim.opt.cmdheight = 0
-end
+-- if vim.fn.has("nvim-0.8") == 1 then
+-- 	vim.opt.cmdheight = 0
+-- end
 
 -- enable spanish spell on markdown only
 -- local markdown_spell = vim.api.nvim_create_augroup("markdownSpell", {})
